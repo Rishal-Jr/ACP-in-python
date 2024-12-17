331148ff6dc3ec300652a0fb2694ba7f92331768
Rishal-Jr <rmrazni@gmail.com>
2024-12-17 19:09:40 +0530

    hh4

---
+++ acp.py
@@ -0,0 +1,10 @@
+a="2nd Jan"
+b="10th May"
+c="25th June"
+d="6th december"
+e="8th November"
+print("RIshal's birthday is on",a)
+print("Razni's birthday is on",b,"My real BD")
+print("Ridhan's birthday is on",c)
+print("Riyall's birthday is on",d)
+print("Risath's birthday is on",e)
